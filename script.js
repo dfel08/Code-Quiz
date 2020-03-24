ad41a4b3d0326d28009bd195667211fc697a9e4f
dfel08 <dfellows68@gmail.com>
2020-03-24 10:03:03 -0700

    post Brad and Jack sessions

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var time = document.querySelector(".time");
+var start = document.querySelector("#start");
+var content = document.querySelector("#content");
+var questionDisplay = document.querySelector("#questionDisplay");
+var answersDisplay = document.querySelector(".answersDisplay");
+var answerDisplay0 = document.querySelector("#answerDisplay0");
+var answerDisplay1 = document.querySelector("#answerDisplay1");
+var answerDisplay2 = document.querySelector("#answerDisplay2");
+var answerDisplay3 = document.querySelector("#answerDisplay3");
+var index = 3;
+var secondsRemaining = 25;
 var questions = [
     {
         question: "Who is considered the person who started the Romantic movement in art?",
@@ -6,7 +17,7 @@ var questions = [
             b: "Taylor Swift",
             c: "Johann Sebastion Bach",
             d: "Ludwig van Beethoven"
-        }
+        },
         correctAnswer: "d"
     },
     {
@@ -16,50 +27,115 @@ var questions = [
             b: "Pikachoo",
             c: "Picachu",
             d: "Pikachu"
-        }
+        },
         correctAnswer: "d"
     },
     {
-        question: "Who invented the toilet?"
+        question: "Who invented the toilet?",
         answers: {
             a: "John Harington",
             b: "Ismail al-Jazari",
             c: "Joseph Bramah",
             d: "All of the above"
-        }
+        },
         correctAnswer: "d"
-    }
+    },
     {
-        question: "Who is Geralt?"
+        question: "Who is Geralt?",
         answers: {
             a: "A human",
             b: "A cow",
-            c: "A mutant",
-            d: "A fish"
-        }
-        correctAnswer: "c"
-    }
+            c: "A fish",
+            d: "A mutant"
+        },
+        correctAnswer: "d"
+    },
     {
-        question: "Is the world round?"
+        question: "Is the world round?",
         answers: {
-            a: "Airplane windows are curved, so no"
-            b: "Have you been skydiving? Of course it's round"
-            c: "BLASPHEMY!!!"
-            d: "There's an ice wall on the edge of the world, it's flat."
-        }
-        correctAnswer: "c"
+            a: "Airplane windows are curved, so no",
+            b: "Have you been skydiving? Of course it's round",
+            c: "There's an ice wall on the edge of the world, it's flat.",
+            d: "BLASPHEMY!!!",
+        },
+        correctAnswer: "d"
         //tehe
     }
 ]
 
-for (var i = 0; i < questions.length; i++) {
-    function startQuiz() {
-        $("div").append(questions[i]);
-        
-    }
-    document.getElementById("start").addEventListener("click", startQuiz);
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsRemaining--;
+        time.textContent = secondsRemaining + " seconds remainging";
+        if (secondsRemaining === 0) {
+            clearInterval(timerInterval);
+            alert("Nice try")
+        }
+    }, 1000);
 }
-//not working
 
+start.addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById("start").style.visibility = "hidden";
+    document.getElementById("questionDisplay").style.visibility = "visible";
+    setTime();
+    quiz(index);
+});
+console.log(Object.values(questions[0].answers))
+function quiz() {
+    questionDisplay.textContent = questions[0].question;
+    var answersArray = [
+        answerDisplay0.textContent = questions[0].answers,
+        answerDisplay1.textContent = questions[1].answers,
+        answerDisplay2.textContent = questions[2].answers,
+        answerDisplay3.textContent = questions[3].answers
+    ];
+    checkAnswer();
+
+    for (var i = 0; i < answersArray.length; i++) {
+        if (i < questions.length) {
+            quiz()
+        } else {
+            endQuiz()
+        }
+    }
+
+
+   
+    
+    if ((questions.correctAnswer) != "d") {
+        secondsLeft -= -10
+        } else {
+        console.log(question.correctAnswer)
+        }
+    }
+    
+    
+    
+    function checkAnswer() {
+      answersDisplay.addEventListener("click", function () {
+        console.log("check answer clicked")
+        var userSelection = event.target;
+        if (userSelection !== questions[index].correctAnswer) {
+          secondsLeft -= -10
+        } else {
+          console.log("correct answer");
+        }
+      });
+    } 
+    
+    
+    function endGame() {
+    }
+
+
+// for (var i = 0; i < questions.length; i++) {
+//     function startQuiz() {
+//         $("div").append(questions[i]);
+
+//     }
+//     document.getElementById("start").addEventListener("click", startQuiz);
+// }
+//not working
 
 
