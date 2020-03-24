35006017994ffab025d3f719a28d03186db56880
dfel08 <dfellows68@gmail.com>
2020-03-23 17:48:53 -0700

    created files linked to jQuery

---
+++ script.js
@@ -0,0 +1,56 @@
+var questions = [
+    {
+        question: "Who is considered the person who started the Romantic movement in art?",
+        answers: {
+            a: "Francisco Goya",
+            b: "Taylor Swift",
+            c: "Johann Sebastion Bach",
+            d: "Ludwig van Beethoven"
+        }
+        correctAnswer: "d"
+    },
+    {
+        question: "Who is Ash Ketchum's favorite pet?",
+        answers: {
+            a: "Picachoo",
+            b: "Pikachoo",
+            c: "Picachu",
+            d: "Pikachu"
+        }
+        correctAnswer: "d"
+    },
+    {
+        question: "Who invented the toilet?"
+        answers: {
+            a: "John Harington",
+            b: "Ismail al-Jazari",
+            c: "Joseph Bramah",
+            d: "All of the above"
+        }
+        correctAnswer: "d"
+    }
+    {
+        question: "Who is Geralt?"
+        answers: {
+            a: "A human",
+            b: "A cow",
+            c: "A mutant",
+            d: "A fish"
+        }
+        correctAnswer: "c"
+    }
+    {
+        question: "Is the world round?"
+        answers: {
+            a: "Airplane windows are curved, so no"
+            b: "Have you been skydiving? Of course it's round"
+            c: "BLASPHEMY!!!"
+            d: "There's an ice wall on the edge of the world, it's flat."
+        }
+        correctAnswer: "c"
+        //tehe
+    }
+]
+
+
+
